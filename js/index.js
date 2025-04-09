d173a054392c257c5dd8fe6e150bc32b70f459ad
Daniel <daniel.seebacher17@gmail.com>
2025-04-09 12:27:47 +0200

    Added the toggling of the answer

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 const bookmark = document.querySelector("[data-js=bookmark]");
 const favorite = document.querySelector("[data-js=favorite]");
 const bookmarkArea = document.querySelector("[data-js=bookmarkArea]");
+const answerButton = document.querySelector("[data-js=showAnswer]");
+const answer = document.querySelector("[data-js=answer]");
 console.log(bookmark);
 console.log(favorite);
 console.log(bookmarkArea);
+console.log(answerButton);
+console.log(answer);
 
 bookmarkArea.addEventListener("click", (event) => {
   console.log(bookmark.classList);
   bookmark.classList.toggle("hidden");
   favorite.classList.toggle("hidden");
+});
+
+answerButton.addEventListener("click", (event) => {
+  console.log(answer.classList);
+  if (answerButton.innerHTML == "Show answer") {
+    answerButton.innerHTML = "Hide answer";
+  } else {
+    answerButton.innerHTML = "Show answer";
+  }
+  answer.classList.toggle("hidden");
 });
